3f6721b0585939cf7cc1c1fc61dc41aa1e5fb502
nickjcroucher <n.croucher@imperial.ac.uk>
2021-06-01 06:42:01 +0100

    Clean up datasets

--- data-raw/preprocess_data.R
+++ data-raw/preprocess_data.R
@@ -1,7 +1,17 @@
+process_pneumo_df<-function(in.df) {
+  in.df %<>%
+    dplyr::select(DS, Serotype, carriage, disease, n.swab, N, time.int) %>%
+    dplyr::rename(study = DS) %>%
+    dplyr::rename(carriage_samples = n.swab) %>%
+    dplyr::rename(surveillance_population = N) %>%
+    dplyr::rename(time_interval = time.int) %>%
+    dplyr::rename(categorisation = Serotype)
+}
+
 # Load raw data from .csv file
 S_pneumoniae_infant_serotype <- read.csv("data-raw/S_pneumoniae_infant_serotype.csv")
 S_pneumoniae_adult_serotype <- read.csv("data-raw/S_pneumoniae_adult_serotype.csv")
 
 # Save the cleaned data in the required R package location
-usethis::use_data(S_pneumoniae_infant_serotype)
-usethis::use_data(S_pneumoniae_adult_serotype)
+usethis::use_data(S_pneumoniae_infant_serotype, overwrite = TRUE)
+usethis::use_data(S_pneumoniae_adult_serotype, overwrite = TRUE)

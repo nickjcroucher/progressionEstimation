3177d62d414726d0b2ca8424b199fd5c66c54f1f
nickjcroucher <n.croucher@imperial.ac.uk>
2021-06-01 08:36:30 +0100

    Use factors in datasets

--- data-raw/preprocess_data.R
+++ data-raw/preprocess_data.R
@@ -1,15 +1,19 @@
-process_pneumo_df<-function(in.df) {
-  in.df %<>%
-    dplyr::select(DS, Serotype, carriage, disease, n.swab, N, time.int) %>%
-    dplyr::rename(study = DS) %>%
-    dplyr::rename(carriage_samples = n.swab) %>%
-    dplyr::rename(surveillance_population = N) %>%
-    dplyr::rename(time_interval = time.int) %>%
-    dplyr::rename(categorisation = Serotype)
+require(tidyverse)
+
+process_pneumo_df<-function(fn) {
+  input_df <-
+    read.csv(fn) %<>%
+      dplyr::select(DS, Serotype, carriage, disease, n.swab, N, time.int) %>%
+      dplyr::rename(study = as.factor(DS)) %>%
+      dplyr::rename(carriage_samples = n.swab) %>%
+      dplyr::rename(surveillance_population = N) %>%
+      dplyr::rename(time_interval = time.int) %>%
+      dplyr::rename(categorisation = as.factor(Serotype))
+  return(input_df)
 }
 
 # Load raw data from .csv file
-S_pneumoniae_infant_serotype <- read.csv("data-raw/S_pneumoniae_infant_serotype.csv")
+S_pneumoniae_infant_serotype <- process_pneumo_df("data-raw/S_pneumoniae_infant_serotype.csv")
 S_pneumoniae_adult_serotype <- read.csv("data-raw/S_pneumoniae_adult_serotype.csv")
 
 # Save the cleaned data in the required R package location

fbd400662707734a2ea0a261ee955947ab887994
nickjcroucher <n.croucher@imperial.ac.uk>
2021-06-02 08:55:18 +0100

    Enable compilation of stan models

---
+++ R/progressionEstimation-package.R
@@ -0,0 +1,14 @@
+#' The 'progressionEstimation' package.
+#'
+#' @description This package uses Bayesian models implemented in stan to estimate the rates at which microbes progress from carriage to disease using case and carrier data.
+#'
+#' @docType package
+#' @name progressionEstimation-package
+#' @aliases progressionEstimation
+#' @useDynLib progressionEstimation, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#'
+NULL

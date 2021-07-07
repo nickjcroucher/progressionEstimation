74009d2074edef9c2db4377890c90dc0e82e33e6
nickjcroucher <n.croucher@imperial.ac.uk>
2021-07-07 13:29:11 +0100

    Update pneumococcal data

--- data-raw/preprocess_data.R
+++ data-raw/preprocess_data.R
@@ -3,12 +3,7 @@ require(tidyverse)
 process_pneumo_df <- function(fn) {
   input_df <-
     read.csv(fn) %>%
-    dplyr::mutate(study = factor(DS)) %>%
-    dplyr::mutate(type = factor(Serotype)) %>%
-    dplyr::rename(carriage_samples = n.swab) %>%
-    dplyr::rename(surveillance_population = N) %>%
-    dplyr::rename(time_interval = time.int) %>%
-    dplyr::select(study, type, carriage_samples, surveillance_population, time_interval, carriage, disease)
+      dplyr::select(study, type, carriage_samples, surveillance_population, time_interval, carriage, disease)
   return(input_df)
 }
 
